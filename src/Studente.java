0ff4de5f2c92f88fee6e54fc6e46905009db718f
fabri <fabri@172.19.160.15>
2024-11-15 10:46:26 +0100

    commit fabrizio get e set di nome e cognome

--- src/Studente.java
+++ src/Studente.java
@@ -25,7 +25,21 @@ public class Studente {
         this.data_nascita = data_nascita;
         this.matricola = matricola;
     }
-    
-    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
     
 }
