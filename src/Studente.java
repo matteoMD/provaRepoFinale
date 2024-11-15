dcf753a442f1d4b0ecbda132567e6c67ab7f5ae7
acest <acestylepoints@gmail.com>
2024-11-15 10:15:49 +0100

    commit matteo attributi e costruttore

--- src/Studente.java
+++ src/Studente.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,20 @@
  * @author acest
  */
 public class Studente {
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+    private LocalDate data_nascita;
+    private String matricola;
+
+    public Studente(String nome, String cognome, String codiceFiscale, LocalDate data_nascita, String matricola) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+        this.data_nascita = data_nascita;
+        this.matricola = matricola;
+    }
+    
+    
     
 }
